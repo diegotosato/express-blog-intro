7b1b593dbc0bee39c12758b98879b9e88f78c3cd
diegotosato <tosato1998@gmail.com>
2025-11-24 11:50:40 +0100

    create the project qith initial file server.js, define the git ignore, install express, write default structure into server.js, verify that the server works

---
+++ server.js
@@ -0,0 +1,19 @@
+//#1 importo express
+const express = require('express')
+
+//#2 creo una variabile per stanziare l'app express
+const app = express()
+
+//#3 definisco la porta dove apro il server
+//buona pratica tenere il nome in maiuscolo per quelle costanti che ratano TALI
+const PORT = 3000
+
+//#4 metto il server in ascolto
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+})
+
+//#5 definisco la prima rotta
+app.get('/', (req, res) => {
+    res.send('My Express blog intro')
+})

a323013dbef9e114b069362483fa1b031ec04a91
diegotosato <tosato1998@gmail.com>
2025-11-24 12:02:27 +0100

    define array to contain the posts, define new route "bacheca", manage the response in json format to return the array posts

--- server.js
+++ server.js
@@ -8,12 +8,61 @@ const app = express()
 //buona pratica tenere il nome in maiuscolo per quelle costanti che ratano TALI
 const PORT = 3000
 
-//#4 metto il server in ascolto
+
+
+
+//array per i post del blog
+const posts = [
+    {
+        id: 1,
+        title: '',
+        content: '',
+        image: '',
+        tags: ['']
+    },
+    {
+        id: 2,
+        title: '',
+        content: '',
+        image: '',
+        tags: ['']
+    },
+    {
+        id: 3,
+        title: '',
+        content: '',
+        image: '',
+        tags: ['']
+    },
+    {
+        id: 4,
+        title: '',
+        content: '',
+        image: '',
+        tags: ['']
+    },
+    {
+        id: 5,
+        title: '',
+        content: '',
+        image: '',
+        tags: ['']
+    }
+]
+
+
+
+// metto il server in ascolto
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
 })
 
-//#5 definisco la prima rotta
+// definisco la prima rotta
 app.get('/', (req, res) => {
     res.send('Server del mio blog')
+})
+
+//definisco la rotta bacheca
+app.get('/bacheca', (req, res) => {
+    res.json(posts)
 })

58e093f0fb3e7537b7f0ae878416ff24e2e9058d
diegotosato <tosato1998@gmail.com>
2025-11-25 12:53:20 +0100

    refactor server.js to define posts array and return JSON response for bacheca route

--- server.js
+++ server.js
@@ -1,19 +1,20 @@
-// importo express
+//#1 importo express
 const express = require('express')
 
-// creo una variabile per stanziare l'app express
+//#2 creo una variabile per stanziare l'app express
 const app = express()
 
-// definisco la porta dove apro il server
+//#3 definisco la porta dove apro il server
 //buona pratica tenere il nome in maiuscolo per quelle costanti che ratano TALI
 const PORT = 3000
 
-//importo il ruuting per la rotta "posts"
-const postsRouter = require('./routers/posts')
 
 
 //vado a dire che la cartella public deve essere un asset statico
 app.use(express.static('public'));
+
+
+
 
 
 // metto il server in ascolto
@@ -26,8 +27,47 @@ app.get('/', (req, res) => {
     res.send('Server del mio blog')
 })
 
+//definisco la rotta bacheca
+app.get('/bacheca', (req, res) => {
 
+    //array per i post del blog
+    const posts = [
+        {
+            id: 1,
+            title: '',
+            content: '',
+            image: '/img/ciambellone.jpeg',
+            tags: ['']
+        },
+        {
+            id: 2,
+            title: '',
+            content: '',
+            image: '/img/cracker_barbabietola.jpeg',
+            tags: ['']
+        },
+        {
+            id: 3,
+            title: '',
+            content: '',
+            image: '/img/pane_fritto_dolce.jpeg',
+            tags: ['']
+        },
+        {
+            id: 4,
+            title: '',
+            content: '',
+            image: '/img/pasta_barbabietola.jpeg',
+            tags: ['']
+        },
+        {
+            id: 5,
+            title: '',
+            content: '',
+            image: '/img/torta_paesana.jpeg',
+            tags: ['']
+        }
+    ]
 
-
-
-app.use('/api/posts', postsRouter)
+    res.json(posts)
+})
